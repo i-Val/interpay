fd4b72095b70a1c04c539e9f159366199e803dcf
i-val <ferdinantino1992@gmail.com>
2024-02-19 15:58:42 +0100

    initial commit

---
+++ src/InterpayServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+class InterpayServiceProvider extends ServiceProvider {
+
+    public function boot() {
+
+    }
+
+    public function register() {
+        
+    }
+}
