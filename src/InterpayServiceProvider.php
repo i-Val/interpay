056e391a80f12ab99de0cec056c7c16cd93fd5e9
i-val <ferdinantino1992@gmail.com>
2024-03-01 15:21:06 +0100

    added config file for keys

--- src/InterpayServiceProvider.php
+++ src/InterpayServiceProvider.php
@@ -7,8 +7,8 @@ class InterpayServiceProvider extends ServiceProvider {
     public function boot() {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->publishes([
-            __DIR__.'/Paystack/config/config.php',
-        ]);
+            __DIR__.'/Paystack/config/config.php' => config_path('interpay.php'),
+        ], 'config');
     }
 
     public function register() {
